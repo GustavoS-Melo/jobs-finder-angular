fce63b1891fbf96fcc6bc747682fe8fc6cfb2051
Gustavo <dev.gustavo97@gmail.com>
2026-02-04 19:38:43 -0300

    refactor: fix favorite UX by moving action to job card

--- src/app/core/layout/header/header.ts
+++ src/app/core/layout/header/header.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, computed } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FavoritesService } from '../../../features/jobs/favorites.service';
 
 @Component({
   selector: 'app-header',
   standalone:true,
-  imports: [RouterLink],
-  template: `<header class="bg-zinc-900 text-white">
-      <div class="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
-        <h1 class="text-xl font-bold">JobsFinder</h1>
+  imports: [RouterModule],
+  template: `
+    <header class="bg-zinc-900 text-white px-6 py-4 flex justify-between items-center">
+      <h1 class="text-xl font-bold">JobsFinder</h1>
 
-        <nav class="flex gap-6">
-          <a routerLink="/" class="hover:text-indigo-400">Home</a>
-          <a routerLink="/jobs" class="hover:text-indigo-400">Vagas</a>
-          <a routerLink="/favorites" class="hover:text-indigo-400">Favoritos</a>
-        </nav>
-      </div>
-    </header>`
+      <nav class="flex items-center gap-6">
+        <a routerLink="/" class="hover:underline">Home</a>
+        <a routerLink="/jobs" class="hover:underline">Vagas</a>
+
+        <a routerLink="/favorites" class="relative text-xl">
+          ❤️
+          @if (favoritesCount() > 0) {
+            <span
+              class="absolute -top-2 -right-3 bg-red-500 text-white text-xs rounded-full px-1.5"
+            >
+              {{ favoritesCount() }}
+            </span>
+          }
+        </a>
+      </nav>
+    </header>
+    `
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private favoritesService = inject(FavoritesService);
+
+  favoritesCount = computed(
+    () => this.favoritesService.favorites().length
+  );
+}
