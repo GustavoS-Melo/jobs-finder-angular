1e8d32a41bb463f9c0fb08beb98b7b9cf368682e
Gustavo <dev.gustavo97@gmail.com>
2026-02-05 14:14:02 -0300

    feat: add empty states for jobs and favorites pages

--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,28 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, inject, computed } from "@angular/core";
 import { JobCardComponent } from '../../features/jobs/job-card/job-card';
 import { FavoritesService } from '../../features/jobs/favorites.service';
-import { computed } from '@angular/core';
 
 @Component({
-  selector: 'app-favorites',
   standalone: true,
   imports: [JobCardComponent],
-  template:  `
-    <h2 class="text-2xl font-bold mb-6">Vagas Favoritas</h2>
+  template: `
+    <h2 class="text-2xl font-bold mb-6">Vagas favoritas</h2>
 
     @if (favorites().length === 0) {
-      <p class="text-gray-600">Nenhuma vaga favoritada ainda.</p>
+      <div class="text-center text-gray-500 mt-20">
+        <p class="text-lg">💔 Nenhuma vaga favoritada ainda</p>
+        <p class="mt-2">Explore vagas e marque suas favoritas</p>
+      </div>
+    } @else {
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        @for (job of favorites(); track job.id) {
+          <app-job-card [job]="job" />
+        }
+      </div>
     }
-
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-      @for (job of favorites(); track job.id) {
-        <app-job-card [job]="job"></app-job-card>
-      }
-    </div>
   `
 })
-export class FavoritesPage {
-  favorites = computed(() => this.favoriteService.favorites());
 
-  constructor(private favoriteService: FavoritesService){}
+export class FavoritesPage {
+  private favoritesService = inject(FavoritesService);
+
+  favorites = computed(() => this.favoritesService.favorites());
 }
--- src/app/pages/jobs/jobs.ts
+++ src/app/pages/jobs/jobs.ts
@@ -4,18 +4,24 @@ import { JobCardComponent } from '../../features/jobs/job-card/job-card';
 import { JOBS_MOCK } from '../../features/jobs/jobs.mock';
 
 @Component({
-  selector: 'app-jobs',
   standalone: true,
   imports: [CommonModule, JobCardComponent],
   template: `
-  <h2 class="text-2xl font-bold mb-6">Vagas disponíveis</h2>
+    <h2 class="text-2xl font-bold mb-6">Vagas disponíveis</h2>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-      @for (job of jobs; track job.id) {
-        <app-job-card [job]="job"></app-job-card>
-      }
-    </div>
-    `
+    @if (jobs.length === 0) {
+      <div class="text-center text-gray-500 mt-20">
+        <p class="text-lg">😕 Nenhuma vaga encontrada</p>
+        <p class="mt-2">Tente ajustar os filtros</p>
+      </div>
+    } @else {
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        @for (job of jobs; track job.id) {
+          <app-job-card [job]="job" />
+        }
+      </div>
+    }
+  `
 })
 export class JobsPage {
   jobs = JOBS_MOCK;
